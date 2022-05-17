3903979c204e96eb9ba8cd5055e7e3c8df7da127
joao <joao.freixial.silveira@tecnico.ulisboa.pt>
2022-05-17 10:13:45 +0100

    updated test script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -3,16 +3,19 @@ then
     make
 fi
 
+pushd tests/
 
-for input_file in tests/*.l22 ; do
-    output_file="tests/expected/`basename ${input_file%.l22}.out`"
+for input_file in *.l22 ; do
+    output_file="expected/`basename ${input_file%.l22}.out`"
     if [ -f $output_file ]; 
     then
-        ./l22 --target asm $input_file
+        ../l22 --target asm $input_file > /dev/null
         asm_file=${input_file%.l22}.asm
-        yasm -felf32 $asm_file
-        bin_file=${input_file%.l22}
-        ld -melf_i386 -o $bin_file $asm_file -lrts
-        ./$bin_file
+        yasm -felf32 $asm_file > /dev/null
+        bin_file=${input_file%.l22}.o
+        ld -melf_i386 -o $bin_file $asm_file -lrts > /dev/null
+        # ./$bin_file
     fi
 done
+
+popd
